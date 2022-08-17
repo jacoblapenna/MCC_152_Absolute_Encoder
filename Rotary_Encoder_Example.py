5d3b6555287c7e0c05b1b586eb72e2fe25073e49
Jacob Lapenna <jlapenna@usbr.gov>
2022-08-17 14:46:14 -0600

    Print re tuple

--- Rotary_Encoder_Example.py
+++ Rotary_Encoder_Example.py
@@ -2,10 +2,16 @@ from daqhats import mcc152, HatError, HatIDs, hat_list
 
 # create hat instance for rotary encoder
 re = mcc152(1)
+re.dio_reset()
+last_position = None
 
 try:
     while True:
         position = re.dio_input_read_port()
-        print("{0} : {0:08b}".format(position))
+        if position == last_position:
+            pass
+        else:
+            print("{0} : {0:08b}".format(position))
+            last_position = position
 except KeyboardInterrupt:
     pass

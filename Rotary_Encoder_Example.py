45aef883c2e425916ca82091d7979b1711017077
Jacob Lapenna <jlapenna@usbr.gov>
2022-08-17 11:55:14 -0600

    Print re tuple

--- Rotary_Encoder_Example.py
+++ Rotary_Encoder_Example.py
@@ -2,3 +2,9 @@ from daqhats import mcc152, HatError, HatIDs, hat_list
 
 # create hat instance for rotary encoder
 re = mcc152(1)
+
+try:
+    while True:
+        print(re.dio_input_read_tuple())
+except KeyboardInterrupt:
+    pass

6174f57337c9fd25c237877e7a98734e590dcd67
Jacob Lapenna <jlapenna@usbr.gov>
2022-08-18 15:03:48 -0600

    Gray code conversion

--- Rotary_Encoder_Example.py
+++ Rotary_Encoder_Example.py
@@ -3,15 +3,22 @@ from daqhats import mcc152, HatError, HatIDs, hat_list
 # create hat instance for rotary encoder
 re = mcc152(1)
 re.dio_reset()
-last_position = None
+last_pos = None
+
+def g2b(num):
+    num ^= num >> 4
+    num ^= num >> 2
+    num ^= num >> 1
+    return num
 
 try:
     while True:
-        position = re.dio_input_read_port()
-        if position == last_position:
+        pos = re.dio_input_read_port()
+        g2b_pos = g2b(pos)
+        if g2b_pos == last_pos:
             pass
         else:
-            print(f"{str(position).ljust(3)}" + " : {0:08b}".format(position))
-            last_position = position
+            print(f"{str(g2b_pos).ljust(3)}" + "Code: {0:08b}".format(pos))
+            last_pos = g2b_pos
 except KeyboardInterrupt:
     pass

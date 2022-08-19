3ad94d0e8d5792f9a75a43169af5466ceee57a01
Jacob Lapenna <jlapenna@usbr.gov>
2022-08-19 15:29:42 -0600

    Hashmap method

--- Rotary_Encoder_Example.py
+++ Rotary_Encoder_Example.py
@@ -3,6 +3,7 @@ from daqhats import mcc152, HatError, HatIDs, hat_list, DIOConfigItem
 # create hat instance for rotary encoder
 re = mcc152(1)
 re.dio_reset()
+# pull-up resistor inverts input
 re.dio_config_write_port(DIOConfigItem.INPUT_INVERT, 255)
 last_pos = None
 
@@ -12,6 +13,7 @@ def g2b(num):
     num ^= num >> 1
     return num
 
+d_theta_degrees = 360/256
 g2b_hashmap = {g : g2b(g) for g in range(256)}
 
 try:
@@ -21,7 +23,13 @@ try:
         if bcd == last_pos:
             pass
         else:
-            print(f"{str(bcd).ljust(3)}" + " : {0:08b}".format(pos))
+            angle = str(round(bcd * d_theta_degrees, 5))
+            theta_whole, theta_decimal = angle.split('.')
+            s = "{0:08b}".format(pos)
+            s += " : "
+            s += f"{theta_whole.rjust(3)}."
+            s += f"{theta_decimal.ljust(5)}"
+            print(s)
             last_pos = bcd
 except KeyboardInterrupt:
     pass
